130981806593c523e8c74cb4b8ea5c96f93d2b89
jshingler <shinglerjim@gmail.com>
2024-12-05 19:50:46 -0500

    Initial Check-In

---
+++ src/test/java/com/example/helloworld/AppTest.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testMain() {
+        assertTrue(true); // Basic test placeholder
+    }
+}
